2bc55e9761af01fb620f1e69721bac452e461c73
Navita4 <navita441992@gmail.com>
2017-07-18 10:24:25 +0530

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,23 @@
+var employee ={
+    name : "Nikki",
+    age : 20,
+    salary : 40000
+};
+    
+ address ={
+    city : "Nagpur",
+        state : "Maharashtra",
+            pincode: 440001
+};
+
+//printing in console
+console.log("name :"+employee.name);
+console.log("age :"+employee.age);
+console.log("salary :"+employee.salary);
+employee.city = "Nagpur";
+console.log(employee.city);
+employee.state = "Maharashtra";
+console.log(employee.state);
+employee.pincode = 440001;
+console.log(employee.pincode);
+
